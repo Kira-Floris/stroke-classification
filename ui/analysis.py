c4aaba818839a6777333d5cd35552de2ac8a8c77
Kira-Floris <nzafloris@gmail.com>
2022-08-15 01:31:53 +0200

    sklearn

--- ui/analysis.py
+++ ui/analysis.py
@@ -26,14 +26,14 @@ def piechart(df, column):
 def filter(df):
 	st.sidebar.header('Filter')
 	gender = st.sidebar.multiselect('Select a Gender', options=list(df['gender'].unique()), default=list(df['gender'].unique()))
-	age = st.sidebar.slider('Select age range', 0.0, df['age'].max(), (0.0,df['age'].max()), key='age')
+	age = st.sidebar.slider('Select age range', 0.0, float(df['age'].max()), (0.0,float(df['age'].max())), key='age')
 	hypertension = st.sidebar.multiselect('Hypertension: 1 for true, 0 for false', options=list(df['hypertension'].unique()), default=list(df['hypertension'].unique()))
 	heart_disease = st.sidebar.multiselect('heart_disease: 1 for true, 0 for false', options=list(df['heart_disease'].unique()), default=list(df['heart_disease'].unique()))
 	ever_married = st.sidebar.multiselect('Select marital status', options=list(df['ever_married'].unique()), default=list(df['ever_married'].unique()))
 	work_type = st.sidebar.multiselect('Select your work type', options=list(df['work_type'].unique()), default=list(df['work_type'].unique()))
 	residence_type = st.sidebar.multiselect('Select your residence type', options=list(df['Residence_type'].unique()), default=list(df['Residence_type'].unique()))
-	avg_glucose_level = st.sidebar.slider('Select average glucose range', 0.0, df['avg_glucose_level'].max(), (0.0,df['avg_glucose_level'].max()), key='avg_glucose_level')
-	bmi = st.sidebar.slider('Select bmi range', 0.0, df['bmi'].max(), (0.0,df['bmi'].max()), key='bmi')
+	avg_glucose_level = st.sidebar.slider('Select average glucose range', 0.0, float(df['avg_glucose_level'].max()), (0.0,float(df['avg_glucose_level'].max())), key='avg_glucose_level')
+	bmi = st.sidebar.slider('Select bmi range', 0.0, float(df['bmi'].max()), (0.0,float(df['bmi'].max())), key='bmi')
 	smoking_status = st.sidebar.multiselect('Select your smoking status', options=list(df['smoking_status'].unique()), default=list(df['smoking_status'].unique()))
 
 	df = df[df['gender'].isin(gender)]
